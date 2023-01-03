986c6dea458200f79ac1e54f776d6618dfd64d94
“[Careen <careennaitore@gmail.com>
2023-01-03 14:24:10 +0300

    create an array

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,6 +1,12 @@
 /**
-  * return number of student
-**/
+ * Retrieves the sum of ids of a list of students.
+ * @param {{
+ *   id: Number,
+ *   firstName: String,
+ *   location: String
+ * }[]} students - The list of students.
+ * @returns {Number}
+ */
 export default function getStudentIdsSum(students) {
   if (students instanceof Array) {
     return students.reduce(
